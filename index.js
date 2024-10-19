b9fb401f65b48f0a92074e0e035345772cd6afab
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2024-10-19 09:35:53 +0300

    Макет, адаптив

---
+++ index.js
@@ -0,0 +1,33 @@
+const buttonUp = document.querySelector(".button-up");
+const buttonDown = document.querySelector(".button-down");
+const table = document.querySelector(".img-active");
+const women = document.querySelector(".women");
+
+// console.log(typeof window.innerWidth);
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 539) {
+    table.style.transform = "scale(1.2)";
+    console.log(true);
+  } else table.style.transform = "scale(1)";
+});
+
+buttonUp.addEventListener("click", () => {
+  if (window.innerWidth <= 320) {
+    table.style.transform = "translateY(-25px) scale(1.2)";
+  } else if (window.innerWidth <= 539) {
+    table.style.transform = "translateY(-57px) scale(1.2)";
+  } else table.style.transform = "translateY(-57px) ";
+});
+
+buttonDown.addEventListener("click", () => {
+  if (window.innerWidth <= 539) {
+    table.style.transform = "translateY(0) scale(1.2)";
+  } else table.style.transform = "translateY(0) scale(1.2)";
+});
+
+women.addEventListener("animationend", () => {
+  if (window.innerWidth === 320) {
+    women.style.animation = "levitate-sm 2s ease-in-out infinite";
+  } else women.style.animation = "levitate 2s ease-in-out infinite";
+});
